9e5c3756b904608aa4fa9257d2f17186077ede2b
Calin Bodnar <calin.bodnar@gmail.com>
2019-08-31 18:26:02 +0100

    Add support for i18n

--- Fitnete/app/onboarding/OnboardingScreen.js
+++ Fitnete/app/onboarding/OnboardingScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import I18n from '../utils/i18n/I18n';
 
 class OnboardingScreen extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Home Screen</Text>
+                <Text>{I18n.t('hello')}</Text>
             </View>
         );
     }
