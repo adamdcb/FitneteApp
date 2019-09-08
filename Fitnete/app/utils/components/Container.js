ddb4046d31c0ec4d564d48e7164a08021fa41b01
Calin Bodnar <calin.bodnar@gmail.com>
2019-09-08 20:14:08 +0100

    Make Container component customisable

--- Fitnete/app/utils/components/Container.js
+++ Fitnete/app/utils/components/Container.js
@@ -4,12 +4,13 @@ import LinearGradient from 'react-native-linear-gradient';
 
 class Container extends React.PureComponent {
     render() {
+        const { locations = [0, 0.5, 1], colors = ['#F3F4FA', '#F9FEFF', '#FFFFFF'], angle = 180, style = {} } = this.props;
         return (
             <LinearGradient
-                style={styles.linearGradient}
-                locations={[0, 0.5138, 1]}
-                colors={['#F3F4FA', '#F9FEFF', '#FFFFFF']}
-                angle={180}
+                style={[styles.linearGradient, style]}
+                locations={locations}
+                colors={colors}
+                angle={angle}
             >
                 {this.props.children}
             </LinearGradient>
