b5fcd6a1bb66364368dcb8bb64ee0bb16fe48745
Calin Bodnar <calin.bodnar@gmail.com>
2019-11-26 19:15:21 +0000

    Save programs on preparing the workout screen

--- Fitnete/app/user-data-input/prepare-workout/PrepareWorkoutPlanPresenter.js
+++ Fitnete/app/user-data-input/prepare-workout/PrepareWorkoutPlanPresenter.js
@@ -1,13 +1,46 @@
+import * as legsProgram from '../../utils/data/legs.json';
+import * as abdominalsProgram from '../../utils/data/abdominals.json';
+import * as armsAndBackProgram from '../../utils/data/armsAndBack.json';
+import * as armsAndChestProgram from '../../utils/data/armsAndChest.json';
+import * as generalProgram from '../../utils/data/general.json';
 import UserDataSource from '../../data/UserDataSource';
+import TrainingDataSource from '../../data/TrainingDataSource.js';
+
+const PROGRAM = {
+    legs: legsProgram,
+    abdominals: abdominalsProgram,
+    arms_and_back: armsAndBackProgram,
+    arms_and_chest: armsAndChestProgram,
+    general: generalProgram
+};
+
+const PROGRAM_SLICE = {
+    0: {
+        start: 0,
+        end: 7
+    },
+    1: {
+        start: 1,
+        end: 6
+    },
+    2: {
+        start: 3,
+        end: 8
+    },
+    3: {
+        start: 4,
+        end: 8
+    }
+}; // fitness level to week start/end index mapping -> FIXME: Should be moved to server side!
 
 export default class PrepareWorkoutPlanPresenter {
     constructor(view) {
         this.view = view;
-        this.dataSource = new UserDataSource();
+        this.userDataSource = new UserDataSource();
+        this.trainingDataSource = new TrainingDataSource();
     }
 
     async startPreparingWorkoutPlan() {
-        const user = await this.dataSource.getUser();
         let progress = 0;
         // TODO: Add real implemenation!
         this.interval = setInterval(() => {
@@ -21,6 +54,20 @@ export default class PrepareWorkoutPlanPresenter {
             }
             this.view.setProgress(progress);
         }, 200);
+
+        try {
+            const user = await this.userDataSource.getUser();
+            await this.trainingDataSource.deleteAllPrograms();
+            const programSlice = PROGRAM_SLICE[user.fitnessLevel];
+            const programs = user.areasOfFocus.reduce((acc, area) => {
+                const program = PROGRAM[area];
+                program.weeks = program.weeks.slice(programSlice.start, programSlice.end)
+                return acc.concat(program);
+            }, [])
+            await this.trainingDataSource.savePrograms(programs);
+        } catch (error) {
+            console.log('startPreparingWorkoutPlan()', error);
+        }
     }
 
     unmountView() {
