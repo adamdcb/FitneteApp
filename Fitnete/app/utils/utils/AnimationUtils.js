2b19b024856320c124546fae8a1baf08f7af3a28
Calin Bodnar <calin.bodnar@gmail.com>
2019-11-28 19:08:28 +0000

    Add new animations to the app

--- Fitnete/app/utils/utils/AnimationUtils.js
+++ Fitnete/app/utils/utils/AnimationUtils.js
@@ -1,89 +1,91 @@
 function getMaleAnimation(exerciseName) {
     switch (exerciseName) {
         case 'squats':
-            return require('../data/exercises/squats-male.webp');
+            return require('../data/exercises/01_Squats_male.webp');
         case 'legSwingRightLeg':
-            return require('../data/exercises/legSwingRightLeg-male.webp');
+            return require('../data/exercises/02_Leg_Swing_R_male.webp');
         case 'legSwingLeftLeg':
-            return require('../data/exercises/legSwingLeftLeg-male.webp');
+            return require('../data/exercises/02_Leg_Swing_L_male.webp');
         case 'gluteBridge':
-            return require('../data/exercises/gluteBridge-male.webp');
+            return require('../data/exercises/03_Glute_Bridge_male.webp');
         case 'innerThighLiftRightLeg':
-            return require('../data/exercises/innerThighLiftRightLeg-male.webp');
+            return require('../data/exercises/04_Inner_Thigh_Lift_R_male.webp');
         case 'innerThighLiftLeftLeg':
-            return require('../data/exercises/innerThighLiftLeftLeg-male.webp');
-        // case 'sideLegLiftRightLeg':
-        //     return require('../data/exercises/sideLegLiftRightLeg-male.webp');
-        // case 'sideLegLiftLeftLeg':
-        //     return require('../data/exercises/sideLegLiftLeftLeg-male.webp');
-        // case 'gluteKickbacksRightLeg':
-        //     return require('../data/exercises/gluteKickbacksRightLeg-male.webp');
-        // case 'gluteKickbacksLeftLeg':
-        //     return require('../data/exercises/gluteKickbacksLeftLeg-male.webp');
+            return require('../data/exercises/04_Inner_Thigh_Lift_L_male.webp');
+        case 'sideLegLiftRightLeg':
+            return require('../data/exercises/05_Side_Leg_Lift_R_male.webp');
+        case 'sideLegLiftLeftLeg':
+            return require('../data/exercises/05_Side_Leg_Lift_L_male.webp');
+        case 'gluteKickbacksRightLeg':
+            return require('../data/exercises/06_Glute_Kickbacks_R_male.webp');
+        case 'gluteKickbacksLeftLeg':
+            return require('../data/exercises/06_Glute_Kickbacks_L_male.webp');
         case 'pulsingLungesLeftLeg':
-            return require('../data/exercises/pulsingLungesLeftLeg-male.webp');
+            return require('../data/exercises/07_Pulsing_Lunges_L_male.webp');
         case 'pulsingLungesRightLeg':
-            return require('../data/exercises/pulsingLungesRightLeg-male.webp');
-        // case 'marchingHip':
-        //     return require('../data/exercises/marchingHip-male.webp');
-        // case 'singleLegHipThrustersRightLeg':
-        //     return require('../data/exercises/singleLegHipThrustersRightLeg-male.webp');
-        // case 'singleLegHipThrustersLeftLeg':
-        //     return require('../data/exercises/singleLegHipThrustersLeftLeg-male.webp');
+            return require('../data/exercises/07_Pulsing_Lunges_R_male.webp');
+        case 'marchingHip':
+            return require('../data/exercises/08_Marching_Hip_male.webp');
+        case 'singleLegHipThrustersRightLeg':
+            return require('../data/exercises/09_Single_Leg_Hip_Thrusters_male.webp');
+        case 'singleLegHipThrustersLeftLeg':
+            return require('../data/exercises/09_Single_Leg_Hip_Thrusters_male.webp');
         case 'oneLegBridgeRightLeg':
-            return require('../data/exercises/oneLegBridgeRightLeg-male.webp');
+            return require('../data/exercises/10_One_Leg_Bridge_R_male.webp');
         case 'oneLegBridgeLeftLeg':
-            return require('../data/exercises/oneLegBridgeLeftLeg-male.webp');
+            return require('../data/exercises/10_One_Leg_Bridge_L_male.webp');
         case 'singleLegDeadliftRightLeg':
-            return require('../data/exercises/singleLegDeadliftRightLeg-male.webp');
+            return require('../data/exercises/11_Single_Leg_Deadlift_R_male.webp');
         case 'singleLegDeadliftLeftLeg':
-            return require('../data/exercises/singleLegDeadliftLeftLeg-male.webp');
+            return require('../data/exercises/11_Single_Leg_Deadlift_L_male.webp');
         case 'plank':
-            return require('../data/exercises/plank-male.webp');
+            return require('../data/exercises/12_Planks_male.webp');
         case 'basicCrunches':
-            return require('../data/exercises/basicCrunches-male.webp');
+            return require('../data/exercises/13_Basic_Crunches_male.webp');
         case 'mountainClimbers':
-            return require('../data/exercises/mountainClimbers-male.webp');
+            return require('../data/exercises/14_Mountain_Climbers_male.webp');
         case 'rollup':
-            return require('../data/exercises/rollup-male.webp');
+            return require('../data/exercises/15_Rollup_male.webp');
         // case 'windshield':
         //     return require('../data/exercises/windshield-male.webp');
         case 'reverseCrunchStraight':
-            return require('../data/exercises/reverseCrunchStraight-male.webp');
+            return require('../data/exercises/17_Reverse_Crunch_Straight_male.webp');
         case 'commandos':
-            return require('../data/exercises/commandos-male.webp');
+            return require('../data/exercises/19_Commandos_male.webp');
         case 'doubleLegLift':
-            return require('../data/exercises/doubleLegLift-male.webp');
+            return require('../data/exercises/20_Double_Leg_Lift_male.webp');
         case 'doubleLegReach':
-            return require('../data/exercises/doubleLegReach-male.webp');
-        // case 'armPlankWithKnees':
-        //     return require('../data/exercises/armPlankWithKnees-male.webp');
+            return require('../data/exercises/21_Double_Leg_Reach_male.webp');
+        case 'armPlankWithKnees':
+            return require('../data/exercises/22_Arm_Plank_with_Knees_male.webp');
         case 'boatpose':
-            return require('../data/exercises/boatpose-male.webp');
+            return require('../data/exercises/23_Boat_pose_male.webp');
         case 'jackKnives':
-            return require('../data/exercises/jackKnives-male.webp');
+            return require('../data/exercises/24_Jack_Knives_male.webp');
         case 'cobraPushUp':
-            return require('../data/exercises/cobraPushUp-male.webp');
+            return require('../data/exercises/25_Cobra_Push_Up_male.webp');
         case 'oneArmPushUpRightArm':
-            return require('../data/exercises/oneArmPushUpRightArm-male.webp');
+            return require('../data/exercises/26_One_Arm_Push_Up_R_male.webp');
         case 'oneArmPushUpLeftArm':
-            return require('../data/exercises/oneArmPushUpLeftArm-male.webp');
+            return require('../data/exercises/26_One_Arm_Push_Up_L_male.webp');
         case 'handPlankReaches':
-            return require('../data/exercises/handPlankReaches-male.webp');
+            return require('../data/exercises/27_Hand_Plank_Reaches_male.webp');
         // case 'plankRightSide':
         //     return require('../data/exercises/plankRightSide-male.webp');
         // case 'plankLeftSide':
         //     return require('../data/exercises/plankLeftSide-male.webp');
-        // case 'kneePushUps':
-        //     return require('../data/exercises/kneePushUps-male.webp');
+        case 'kneePushUps':
+            return require('../data/exercises/29_Knee_Push_Ups_male.webp');
         // case 'shouldPushUps':
         //     return require('../data/exercises/shouldPushUps-male.webp');
+        case 'pushUp':
+            return require('../data/exercises/30_Push_Ups_male.webp');
         case 'bentOverRow':
-            return require('../data/exercises/bentOverRow-male.webp');
+            return require('../data/exercises/31_Bent_Over_Row_male.webp');
         case 'lateralRaises':
-            return require('../data/exercises/lateralRaises-male.webp');
-        // case 'tricepExtension':
-        //     return require('../data/exercises/tricepExtension-male.webp');
+            return require('../data/exercises/32_Lateral_Raise_male.webp');
+        case 'tricepExtension':
+            return require('../data/exercises/33_Tricep_Extension_R_male.webp');
         // case 'dumbellSwing':
         //     return require('../data/exercises/dumbellSwing-male.webp');
         // case 'squatandCurl':
@@ -110,8 +112,7 @@ function getMaleAnimation(exerciseName) {
         //     return require('../data/exercises/mcGillCurlUp-male.webp');
         // case 'birdDog':
         //     return require('../data/exercises/birdDog-male.webp');
-        case 'pushUp':
-            return require('../data/exercises/pushUp-male.webp');
+        
         // case 'tricepDip':
         //     return require('../data/exercises/tricepDip-male.webp');
         // case 'wallSit':
@@ -122,96 +123,98 @@ function getMaleAnimation(exerciseName) {
         //     return require('../data/exercises/burpees-male.webp');
 
         default:
-            return require('../data/exercises/squats-male.webp');
+            return require('../data/exercises/01_Squats_male.webp');
     }
 }
 
 function getFemaleAnimation(exerciseName) {
     switch (exerciseName) {
         case 'squats':
-            return require('../data/exercises/squats-female.webp');
+            return require('../data/exercises/01_Squats_female.webp');
         case 'legSwingRightLeg':
-            return require('../data/exercises/legSwingRightLeg-female.webp');
+            return require('../data/exercises/02_Leg_Swing_R_female.webp');
         case 'legSwingLeftLeg':
-            return require('../data/exercises/legSwingLeftLeg-female.webp');
+            return require('../data/exercises/02_Leg_Swing_L_female.webp');
         case 'gluteBridge':
-            return require('../data/exercises/gluteBridge-female.webp');
+            return require('../data/exercises/03_Glute_Bridge_female.webp');
         case 'innerThighLiftRightLeg':
-            return require('../data/exercises/innerThighLiftRightLeg-female.webp');
+            return require('../data/exercises/04_Inner_Thigh_Lift_R_female.webp');
         case 'innerThighLiftLeftLeg':
-            return require('../data/exercises/innerThighLiftLeftLeg-female.webp');
-        // case 'sideLegLiftRightLeg':
-        //     return require('../data/exercises/sideLegLiftRightLeg-female.webp');
-        // case 'sideLegLiftLeftLeg':
-        //     return require('../data/exercises/sideLegLiftLeftLeg-female.webp');
-        // case 'gluteKickbacksRightLeg':
-        //     return require('../data/exercises/gluteKickbacksRightLeg-female.webp');
-        // case 'gluteKickbacksLeftLeg':
-        //     return require('../data/exercises/gluteKickbacksLeftLeg-female.webp');
+            return require('../data/exercises/04_Inner_Thigh_Lift_L_female.webp');
+        case 'sideLegLiftRightLeg':
+            return require('../data/exercises/05_Side_Leg_Lift_R_female.webp');
+        case 'sideLegLiftLeftLeg':
+            return require('../data/exercises/05_Side_Leg_Lift_L_female.webp');
+        case 'gluteKickbacksRightLeg':
+            return require('../data/exercises/06_Glute_Kickbacks_R_female.webp');
+        case 'gluteKickbacksLeftLeg':
+            return require('../data/exercises/06_Glute_Kickbacks_L_female.webp');
         case 'pulsingLungesLeftLeg':
-            return require('../data/exercises/pulsingLungesLeftLeg-female.webp');
+            return require('../data/exercises/07_Pulsing_Lunges_L_female.webp');
         case 'pulsingLungesRightLeg':
-            return require('../data/exercises/pulsingLungesRightLeg-female.webp');
-        // case 'marchingHip':
-        //     return require('../data/exercises/marchingHip-female.webp');
-        // case 'singleLegHipThrustersRightLeg':
-        //     return require('../data/exercises/singleLegHipThrustersRightLeg-female.webp');
-        // case 'singleLegHipThrustersLeftLeg':
-        //     return require('../data/exercises/singleLegHipThrustersLeftLeg-female.webp');
+            return require('../data/exercises/07_Pulsing_Lunges_R_female.webp');
+        case 'marchingHip':
+            return require('../data/exercises/08_Marching_Hip_female.webp');
+        case 'singleLegHipThrustersRightLeg':
+            return require('../data/exercises/09_Single_Leg_Hip_Thrusters_female.webp');
+        case 'singleLegHipThrustersLeftLeg':
+            return require('../data/exercises/09_Single_Leg_Hip_Thrusters_female.webp');
         case 'oneLegBridgeRightLeg':
-            return require('../data/exercises/oneLegBridgeRightLeg-female.webp');
+            return require('../data/exercises/10_One_Leg_Bridge_R_female.webp');
         case 'oneLegBridgeLeftLeg':
-            return require('../data/exercises/oneLegBridgeLeftLeg-female.webp');
+            return require('../data/exercises/10_One_Leg_Bridge_L_female.webp');
         case 'singleLegDeadliftRightLeg':
-            return require('../data/exercises/singleLegDeadliftRightLeg-female.webp');
+            return require('../data/exercises/11_Single_Leg_Deadlift_R_female.webp');
         case 'singleLegDeadliftLeftLeg':
-            return require('../data/exercises/singleLegDeadliftLeftLeg-female.webp');
+            return require('../data/exercises/11_Single_Leg_Deadlift_L_female.webp');
         case 'plank':
-            return require('../data/exercises/plank-female.webp');
+            return require('../data/exercises/12_Planks_female.webp');
         case 'basicCrunches':
-            return require('../data/exercises/basicCrunches-female.webp');
+            return require('../data/exercises/13_Basic_Crunches_female.webp');
         case 'mountainClimbers':
-            return require('../data/exercises/mountainClimbers-female.webp');
+            return require('../data/exercises/14_Mountain_Climbers_female.webp');
         case 'rollup':
-            return require('../data/exercises/rollup-female.webp');
+            return require('../data/exercises/15_Rollup_female.webp');
         // case 'windshield':
         //     return require('../data/exercises/windshield-female.webp');
         case 'reverseCrunchStraight':
-            return require('../data/exercises/reverseCrunchStraight-female.webp');
+            return require('../data/exercises/17_Reverse_Crunch_Straight_female.webp');
         case 'commandos':
-            return require('../data/exercises/commandos-female.webp');
+            return require('../data/exercises/19_Commandos_female.webp');
         case 'doubleLegLift':
-            return require('../data/exercises/doubleLegLift-female.webp');
+            return require('../data/exercises/20_Double_Leg_Lift_female.webp');
         case 'doubleLegReach':
-            return require('../data/exercises/doubleLegReach-female.webp');
-        // case 'armPlankWithKnees':
-        //     return require('../data/exercises/armPlankWithKnees-female.webp');
+            return require('../data/exercises/21_Double_Leg_Reach_female.webp');
+        case 'armPlankWithKnees':
+            return require('../data/exercises/22_Arm_Plank_with_Knees_female.webp');
         case 'boatpose':
-            return require('../data/exercises/boatpose-female.webp');
+            return require('../data/exercises/23_Boat_pose_female.webp');
         case 'jackKnives':
-            return require('../data/exercises/jackKnives-female.webp');
+            return require('../data/exercises/24_Jack_Knives_female.webp');
         case 'cobraPushUp':
-            return require('../data/exercises/cobraPushUp-female.webp');
+            return require('../data/exercises/25_Cobra_Push_Up_female.webp');
         case 'oneArmPushUpRightArm':
-            return require('../data/exercises/oneArmPushUpRightArm-female.webp');
+            return require('../data/exercises/26_One_Arm_Push_Up_R_female.webp');
         case 'oneArmPushUpLeftArm':
-            return require('../data/exercises/oneArmPushUpLeftArm-female.webp');
+            return require('../data/exercises/26_One_Arm_Push_Up_L_female.webp');
         case 'handPlankReaches':
-            return require('../data/exercises/handPlankReaches-female.webp');
+            return require('../data/exercises/27_Hand_Plank_Reaches_female.webp');
         // case 'plankRightSide':
         //     return require('../data/exercises/plankRightSide-female.webp');
         // case 'plankLeftSide':
         //     return require('../data/exercises/plankLeftSide-female.webp');
-        // case 'kneePushUps':
-        //     return require('../data/exercises/kneePushUps-female.webp');
+        case 'kneePushUps':
+            return require('../data/exercises/29_Knee_Push_Ups_female.webp');
         // case 'shouldPushUps':
         //     return require('../data/exercises/shouldPushUps-female.webp');
+        case 'pushUp':
+            return require('../data/exercises/30_Push_Ups_female.webp');
         case 'bentOverRow':
-            return require('../data/exercises/bentOverRow-female.webp');
+            return require('../data/exercises/31_Bent_Over_Row_female.webp');
         case 'lateralRaises':
-            return require('../data/exercises/lateralRaises-female.webp');
-        // case 'tricepExtension':
-        //     return require('../data/exercises/tricepExtension-female.webp');
+            return require('../data/exercises/32_Lateral_Raise_female.webp');
+        case 'tricepExtension':
+            return require('../data/exercises/33_Tricep_Extension_R_female.webp');
         // case 'dumbellSwing':
         //     return require('../data/exercises/dumbellSwing-female.webp');
         // case 'squatandCurl':
@@ -238,8 +241,7 @@ function getFemaleAnimation(exerciseName) {
         //     return require('../data/exercises/mcGillCurlUp-female.webp');
         // case 'birdDog':
         //     return require('../data/exercises/birdDog-female.webp');
-        case 'pushUp':
-            return require('../data/exercises/pushUp-female.webp');
+        
         // case 'tricepDip':
         //     return require('../data/exercises/tricepDip-female.webp');
         // case 'wallSit':
@@ -250,7 +252,7 @@ function getFemaleAnimation(exerciseName) {
         //     return require('../data/exercises/burpees-female.webp');
 
         default:
-            return require('../data/exercises/squats-female.webp');
+            return require('../data/exercises/01_Squats_female.webp');
     }
 }
 
