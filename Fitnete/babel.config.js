f4284b56597933f5f50331eeaadfa7924721fe85
Calin Bodnar <calin.bodnar@gmail.com>
2020-01-18 19:21:59 +0000

    Disable console logs in production

--- Fitnete/babel.config.js
+++ Fitnete/babel.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = api => {
+  const env = api.env();
+  const plugins = [];
+  if (env !== 'development') {
+    plugins.push('transform-remove-console');
+  }
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins
+  }
 };
