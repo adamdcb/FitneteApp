9afab7a45f10eba7a004c23a27a64434fc9b2c0c
Calin Bodnar <calin.bodnar@gmail.com>
2020-02-18 18:22:28 +0000

    Add support for Bugsnag (setup, init, fastlane)

--- Fitnete/index.js
+++ Fitnete/index.js
@@ -3,7 +3,10 @@
  */
 
 import { AppRegistry } from 'react-native';
+import { Client } from 'bugsnag-react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+new Client("0f31065ba6a8f376650644cb481c4420");
 
 AppRegistry.registerComponent(appName, () => App);
